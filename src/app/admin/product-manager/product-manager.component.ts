208a99f8b3ab0794d9b5fbfb3ed88071d211e6c6
Thinhncph07423 <nguyenthinh09062000@gmail.com>
2020-04-19 15:07:01 +0700

    30 files added, 29 deleted

--- src/app/admin/product-manager/product-manager.component.ts
+++ src/app/admin/product-manager/product-manager.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Product } from '../Product';
-import { ProductService } from '../services/product.service';
+import { Product } from '../../Product';
+import { ProductService } from '../../services/product.service';
 
 @Component({
   selector: 'app-product-manager',
